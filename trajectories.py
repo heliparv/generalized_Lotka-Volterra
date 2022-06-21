88050101561a1499f0ce112f58a6301056c4025d
Heli Parviainen <heli.susanna.parviainen@gmail.com>
2022-06-21 17:00:03 +0300

    Add alternate functions for calculating trajectories

--- trajectories.py
+++ trajectories.py
@@ -7,11 +7,13 @@ only_viable_trajectories: Generates bacteria abundances as a function of time ba
 Lotka-Volterra dynamics. If abundance of some species drops to zero or below the community is
 deemed non-viable, the simulation aborts and returns "-1"
 
-#TODO: trajectories: Same as previous but when bacterial abundances run negative, function lets
+trajectories_with_extinction: Same as previous but when bacterial abundances run negative, function lets
 species go extinct and continues simulation to given maximum time.
 
-"""
+test_trajectories: No control for negative or zero values, useful for testing how different starting
+values affect the calculated abundances.
 
+"""
 
 #TODO separate birth and death rate in model
 #TODO take lag times into account
@@ -24,14 +26,40 @@ def only_viable_trajectories(n, maxtime, interactions, ri, starting_abundances):
     while time < maxtime+1:
         for species in range(0, n):
             change_per_capita = ri[species] + sum(abundances[time-1]*interactions[species])
-            change = abundances[species]*change_per_capita
-            abundances[time][species] = sum(abundances[species]+change)
-            if abundances[time][species] <= 0:
+            new_abundance = abundances[species] + abundances[species]*change_per_capita
+            if new_abundance <= 0:
                 unviable = True
                 break
+            else:
+                abundances[time][species] = new_abundance
         if unviable:
             return(-1)
         time += 1
     return abundances
 
-#TODO
+def trajectories_with_extinction(n, maxtime, interactions, ri, starting_abundances):
+    abundances = np.zeros((maxtime+1, n))
+    abundances[0] = starting_abundances
+    time = 1
+    while time < maxtime+1:
+        for species in range(0, n):
+            change_per_capita = ri[species] + sum(abundances[time-1]*interactions[species])
+            new_abundance = abundances[species] + abundances[species]*change_per_capita
+            if new_abundance < 0:
+                abundances[time][species] = 0
+            else:
+                abundances[time][species] = new_abundance
+        time += 1
+    return abundances
+
+def test_trajectories(n, maxtime, interactions, ri, starting_abundances):
+    abundances = np.zeros((maxtime+1, n))
+    abundances[0] = starting_abundances
+    time = 1
+    while time < maxtime+1:
+        for species in range(0, n):
+            change_per_capita = ri[species] + sum(abundances[time-1]*interactions[species])
+            new_abundance = abundances[species] + abundances[species]*change_per_capita
+            abundances[time][species] = new_abundance
+        time += 1
+    return abundances
