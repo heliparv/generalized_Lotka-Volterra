685da51e31524887ede3c436ca52bfc05f580e91
Heli Parviainen <heli.susanna.parviainen@gmail.com>
2022-06-21 15:41:03 +0300

    Add function for drawing line charts of abundances

--- graphics.py
+++ graphics.py
@@ -1,2 +1,20 @@
-#TODO add function for drawing trajectories
+import matplotlib.pyplot as plt
+import numpy as np
+from sqlalchemy import null
+
+""" Tools for graphic representations of the simulated data.
+
+functions:
+abundances_line_chart: Draws a line chart of the given abundance data.
+"""
+
 #TODO add function for a heatmap of species interactions
+
+def abundances_line_chart(n, maxtime, abundances):
+    y = range(0, maxtime+1)
+    plt.figure()
+    plt.plot(abundances, y)
+    plt.xlabel("Time")
+    plt.ylabel("Abundance")
+    plt.legend(range(1,n+1), title="Species", loc="center left", fontsize='small', bbox_to_anchor=(1, 0.5))
+    return plt.show()
--- index.py
+++ index.py
@@ -2,14 +2,17 @@ import numpy as np
 import pandas as pd
 from parameters import generate_growth_rates, generate_pairwise_interactions, generate_starting_abundances
 from trajectories import only_viable_trajectories
+from graphics import abundances_line_chart
 
 """Index for performing simulations based on generalized Lotka-Volterra dynamics. Calls appropriate
 functions for generating interaction values, starting abundances and for generating trajectories.
-To modify simulations, modify starting values for functions.
+To modify simulations, modify input values for functions.
 """
 
 #Number of species
 n = 5
+#Maximum simulation time
+maxtime = 15
 
 pairwise_interactions = generate_pairwise_interactions(n)
 
@@ -17,6 +20,9 @@ ri = generate_growth_rates(n)
 
 starting_abundances = generate_starting_abundances(n)
 
-trajectories = only_viable_trajectories(n, 15, pairwise_interactions, ri, starting_abundances)
+abundances = only_viable_trajectories(n, maxtime, pairwise_interactions, ri, starting_abundances)
 
-print(trajectories)
+if abundances == -1:
+    print("Community not viable")
+else:
+    abundances_line_chart(n, maxtime, abundances)
