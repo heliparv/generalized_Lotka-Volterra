c3994073f04c11f6d38cd409638684336640c3a5
Heli Parviainen <heli.susanna.parviainen@gmail.com>
2022-07-26 09:23:09 +0300

    Add hill coefficient to antibiotic functions

--- antibiotics.py
+++ antibiotics.py
@@ -3,10 +3,10 @@ from random import sample
 
 """Functions to add the effect of an antibiotic to the simulation
 
-draw_antibiotic_effect: randomly draws the maximum possible effect of antibiotic
-and the Michaelis-Menten constant for each species from normal distributions
-with means and standard deviantions as specified in input. Uses given antibiotic
-concentration and Michaelis-Menten kinetics to calculate antibiotic effect
+draw_antibiotic_effect: randomly draws the maximum possible effect of antibiotic,
+the Michaelis-Menten constant, and the hill coefficient for each species from normal
+distributions with means and standard deviantions as specified in input. Uses given
+antibiotic concentration and Michaelis-Menten kinetics to calculate antibiotic effect
 at given concentration for the species
 
 add_antibiotic_effect_to_ri: Inputs are number of species, growth rates for species,
@@ -17,20 +17,21 @@ the effect to the growth rates.
 
 """
 
-def draw_antibiotic_effect(n, meanmax, stdmax, meanK, stdK, abconc):
-    max = -abs(np.random.normal(loc=meanmax, scale=abs(meanmax*stdmax), size=n))
+def draw_antibiotic_effect(n, abconc, meanK, stdK, meanmax=-1, stdmax=0.1, meanhill=1, stdhill=0):
     Km = abs(np.random.normal(loc=meanK, scale=meanK*stdK, size=n))
+    max = -abs(np.random.normal(loc=meanmax, scale=abs(meanmax*stdmax), size=n))
+    hill_coefficient = abs(np.random.normal(loc=meanhill, scale=meanhill*stdhill, size=n))
     eff = np.zeros(n)
     for i in range(0, n):
-        eff[i] = max[i]*abconc/(Km[i]+abconc)
+        eff[i] = max[i]*(abconc**hill_coefficient)/((Km[i]**hill_coefficient)+(abconc**hill_coefficient))
     return eff
 
-def add_antibiotic_effect_to_ri(n, ri, meanmax, stdmax, meanK, stdK, abconc, sparcity):
+def add_antibiotic_effect_to_ri(n, ri, abconc, meanK, stdK, meanmax=-1, stdmax=0.1, meanhill=1, stdhill=0, sparcity=0):
     if sparcity < 1:
         number_effected = int(n*sparcity)
     else:
         number_effected = n-int(sparcity)
-    eff = draw_antibiotic_effect(number_effected, meanmax, stdmax, meanK, stdK, abconc)
+    eff = draw_antibiotic_effect(number_effected, abconc, meanK, stdK, meanmax, stdmax, meanhill, stdhill)
     effected_species = sample(range(n), number_effected)
     for i in range(number_effected):
         ri[effected_species[i]] += eff[i]
