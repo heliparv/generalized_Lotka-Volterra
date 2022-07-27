1f6fdd57f93b748afee59b2cbf52fdf863a71a7b
GVarv <71629423+GVarv@users.noreply.github.com>
2022-07-27 11:13:38 +0300

    Update index_gLVwK.py

--- index_gLVwK.py
+++ index_gLVwK.py
@@ -3,6 +3,7 @@ import pandas as pd
 from parameters import add_sparcity, generate_growth_rates, generate_interactions, generate_starting_abundances, adjust_selfinteractions, calculate_carrying_capacities
 from simulations_gLVwK import only_viable_gLVwK, gLVwK_with_extinction, test_gLVwK
 from graphics import abundances_line_chart, interactions_heatmap
+import random
 
 """Index for performing simulations based on generalized Lotka-Volterra dynamics. Calls appropriate
 functions for generating interaction values, starting abundances and for generating trajectories.
@@ -18,9 +19,9 @@ n = 10
 maxtime = 100
 total_carrying_capacity = 20000000
 
-ri = generate_growth_rates(n, 0.5, 0.1)
+ri = generate_growth_rates(n, 0.5, seed_growth=12,std=0.1)
 
-starting_abundances = generate_starting_abundances(n, 100, 0.1)
+starting_abundances = generate_starting_abundances(n,seed_abundance=12, mean=100, std=0.1)
 
 pairwise_interactions = generate_interactions(n, 1, 0, 0.001)
 
