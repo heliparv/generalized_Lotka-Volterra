e22102f8246fe79c7bb1fbffa05ae4b5617a2698
GVarv <71629423+GVarv@users.noreply.github.com>
2022-07-27 14:09:51 +0300

    Update index_cyclic_games_gLVwK.py

--- index_cyclic_games_gLVwK.py
+++ index_cyclic_games_gLVwK.py
@@ -28,13 +28,13 @@ ri = generate_growth_rates(n, 0.5,seed_growth=12, std=0.1)
 
 starting_abundances = generate_starting_abundances(n, seed_abundance=12, mean=100, std=0.1)
 
-pairwise_interactions = generate_interactions(n, 1, 0, 0.001)
+pairwise_interactions = generate_interactions(n, 1,seed_interactions=12,seed_sparcity=12, mean=0, std=0.001)
 
-pairwise_interactions = add_sparcity(pairwise_interactions, 0.3, seed_sparcity=12)
+pairwise_interactions = add_sparcity(pairwise_interactions, 0.3,seed_sparcity=12)
 
 pairwise_interactions = generalized_rps(pairwise_interactions, 6, 2, even_groups_for_rps,seed_groups=12,seed_sparcity=12, sparcity=0.2, interaction_std=0.1)
 
-pairwise_interactions = adjust_selfinteractions(n, pairwise_interactions, -0.001, 0.1)
+pairwise_interactions = adjust_selfinteractions(n, pairwise_interactions,seed_selfinter=12, mean=-0.001, std=0.1)
 
 carrying_capacities = calculate_carrying_capacities(ri, pairwise_interactions)
 
