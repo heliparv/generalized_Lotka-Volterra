be09822e362534c9feed1738e1c2e7f4ee0dbee8
Heli Parviainen <heli.susanna.parviainen@gmail.com>
2022-08-09 11:24:21 +0300

    Add function to create relative abundances dataset

---
+++ data_modification.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+"""
+A collection of functions used for modifying the data or analyzing the data.
+
+relative_abundances: Input is an array of absolute abundances of different
+species at given time points. Returns an array with relative abundances
+"""
+
+def relative_abundances(abundances):
+    sums = np.sum(abundances, 1)
+    return abundances/sums[:,None]
